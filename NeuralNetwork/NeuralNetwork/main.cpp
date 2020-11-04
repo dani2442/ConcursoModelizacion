2800e9eb5e9e64d56cf4a3968920a1a8547c6199
dani2442 <daniel.l.m.2442.123@gmail.com>
2020-11-04 20:49:44 +0100

    main

--- NeuralNetwork/NeuralNetwork/main.cpp
+++ NeuralNetwork/NeuralNetwork/main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
 
+/*
+Project Hierarchy
+
+
+- NeuralNet
+	- Layer
+		- FullyConnected
+		- Convolution
+		- Pooling
+	- Activation (functions)
+		- Sigmoid 
+		- Tanh
+		- Softmax
+		- ReLU
+	- Output
+	- Optimizers
+		- SGD (Stochastic Gradient Descent)
+
+-Random
+-Matrix
+
+
+
+*/
+
+
+
 typedef double Scalar;
 
 int main() {
