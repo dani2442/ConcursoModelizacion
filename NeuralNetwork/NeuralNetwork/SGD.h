5673d92de60be92a7b50c7f6261db54f9cd50d81
dani2442 <daniel.l.m.2442.123@gmail.com>
2020-11-07 20:22:23 +0100

    hallo

--- NeuralNetwork/NeuralNetwork/SGD.h
+++ NeuralNetwork/NeuralNetwork/SGD.h
@@ -1,8 +1,16 @@
 #pragma once
 #include "Optimizer.h"
+#include "Config.h"
 
 // Stockastic Gradient Descent
 class SGD: public Optimizer
 {
+public:
+	Scalar l_rate; // Learning rate
+	SGD(Scalar l_rate=0.01):l_rate(l_rate){}
+
+	void update(ConstAlignedMapVec& dvec, AlignedMapVec& vec) {
+		vec.noalias() -= l_rate*dvec;
+	}
 };
 
