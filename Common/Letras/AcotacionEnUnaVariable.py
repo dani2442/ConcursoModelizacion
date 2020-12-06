54765cbe8d72f0d9b8ae0dc296bed820361b5156
Antimateri <73721533+Antimateri@users.noreply.github.com>
2020-12-06 20:32:29 +0100

    ahora resuelve (hooray!!!)

--- Common/Letras/AcotacionEnUnaVariable.py
+++ Common/Letras/AcotacionEnUnaVariable.py
@@ -1,4 +1,5 @@
 from mykernel import *
+from math import log
 
 def n(letra,m):
     if(m==0):
@@ -8,7 +9,7 @@ def n(letra,m):
         max=0
         min=10
         for i in range(10,20):
-            compara=numero_a_letras(i).count(letra)
+            compara=conteo_numero(i,letra)[letra]
             
             if(letra=='i' and i>15 and i<30):compara+=1
             
@@ -17,7 +18,7 @@ def n(letra,m):
             if(compara<min):
                 min=compara
         for i in range(2,10):
-            compara=numero_a_letras((10)*i).count(letra)
+            compara=conteo_numero((10)*i,letra)[letra]
             
             if(letra=='e' and i>2 and i<3):compara-=1
             if(letra=='i' and i>2 and i<3):compara+=1
@@ -33,7 +34,7 @@ def n(letra,m):
         max=0
         min=10**m
         for i in range(1,10):
-            compara=numero_a_letras((10**(m-1))*i).count(letra)
+            compara=conteo_numero((10**(m-1))*i,letra)[letra]
             
             if( (letra=='t' or letra=='o') and i==1):compara+=1
             
@@ -48,3 +49,14 @@ def cota(m=6, letras='abcdefghijklmnñopqrstuvwxyz'):
     for i in letras:
         out.append((i,n(i,m)))
     return out
+
+def resolver(mensaje, letra):
+    n0=firmar_mensaje_fija(estandarizar_mensaje(mensaje),'e').count(letra)
+    n1=n0+n(letra, int(log(n0,10)+1) )
+    cota=n(letra,int(log(n1,10)+1))
+    for i in range(cota+1):
+        if(-i+conteo_numero(n0+i)[letra]==0):
+            fin='ces'
+            if(n0+i==1):fin='z'
+            return f"En este mensaje aparece {n0+i} ve{fin} la letra e"
+    return "No hay solucion"
