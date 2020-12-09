12a88f6801a73216687a5c0d3970e198b473cbad
Daniel <daniel.l.m.2442.123@gmail.com>
2020-12-09 22:18:26 +0100

    bla bla

--- Common/Letras/prue.py
+++
@@ -1,6 +0,0 @@
-from scipy import ndimage, misc
-import matplotlib.pyplot as plt
-import numpy as np
-
-a=np.array((4,0,3,1,10,0,0,1,2,0,0,1,1,9,0,4,0,1,1,2,4,3,5,0,0,2,1))
-print(np.dot(a,a))
--- Common/Letras/visualization.py
+++ Common/Letras/visualization.py
@@ -4,6 +4,13 @@ from scipy import ndimage, misc
 
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
+
+
+"""
+Nos encargamos en este fichero de la visualización de los datos
+"""
+
+
 mensaje_global="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 def example1(mensaje):
